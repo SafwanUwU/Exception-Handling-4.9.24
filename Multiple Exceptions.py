e65f4816f5070cf65381fcb975ebc5a60cb6653d
Safwan Ahmad <safwanahmad787@gmail.com>
2024-09-04 11:37:14 +0600

    hellow

---
+++ Multiple Exceptions.py
@@ -0,0 +1,17 @@
+try:
+    num1 = int(input("Enter a number:  "))
+    num2 = int(input("Enter a number:  "))
+    result = num1/num2
+
+    print("Result is : ", result)
+    print("Result is : ", result1)
+
+except ZeroDivisionError:
+    print("Division by zero is not allowed")
+except ValueError:
+    print("Please enter a numerical Value")
+except NameError as ex:
+    print("The exception is ", ex)
+
+finally:
+    print("I will execute no matter what happens")
